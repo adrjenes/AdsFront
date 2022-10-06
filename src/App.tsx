c204564266e824da0b933a3bb38473e94d5c2a9f
Adrian <adrjeness@gmail.com>
2022-10-06 10:21:08 +0200

    App structure divided into basic components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Header} from "./components/layout/Header";
 import {Map} from "./components/Map/Map";
 
 import logo from './logo.svg';
+import {SearchContext} from "./contexts/search-context";
 
 export const App = () => {
-  return (
-      <>
+
+    const [search, setSearch] = useState('');
+
+    return (
+      <SearchContext.Provider value={{search, setSearch}}>
           <Header/>
           <Map/>
-      </>
+      </SearchContext.Provider>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
